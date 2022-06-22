382f5db40c27a2d6a3702ef76c961c5942557099
Anamika Gawandi <anamika.gawandi@quantiphi.com>
2022-06-22 20:57:56 +0530

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: true,
+  env: { },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.feature',
+    video: false
+  },
+})
